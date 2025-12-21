36b5a926fccf99324e26364df6fcb95c6e2bb720
SimonAsmar <simon.asmar@polymtl.ca>
2025-12-21 18:49:36 -0500

    fixed TMDS encoder

--- TMDS_encoder/src/TMDS_encoder.vhd
+++ TMDS_encoder/src/TMDS_encoder.vhd
@@ -26,8 +26,8 @@ end TMDS_encoder;
 architecture TMDS_encoder_arch of TMDS_encoder is
   signal Cnt : integer range -32 to 31 := 0; --disparity
 
-  function getNbrOfOnes(data: std_logic_vector) return positive is 
-  variable count := 0;
+  function getNbrOfOnes(data: std_logic_vector) return natural is 
+  variable count : natural := 0;
   begin
   for i in data'range loop
     if data(i) = '1' then
@@ -40,8 +40,8 @@ architecture TMDS_encoder_arch of TMDS_encoder is
 begin
   process(clk, rst)
   variable q_m : std_logic_vector(9 downto 0);
-  variable N_1_D : integer range 0 to 8 := 0;
-  variable N_1_q_m : integer range 0 to 8 := 0;
+  variable N_1_D : natural range 0 to 8 := 0;
+  variable N_1_q_m : natural range 0 to 8 := 0;
 
   begin
     if rising_edge(clk) then
@@ -52,25 +52,25 @@ begin
         N_1_D := getNbrOfOnes(D);
         if N_1_D > 4 OR (N_1_D = 4 AND D(0) = '0') then 
           --XNOR branch
-          q_m(0) = D(0);
+          q_m(0) := D(0);
           for i in 1 to 7 loop
-            q_m(i) = q_m(i-1) XNOR D(i);
+            q_m(i) := q_m(i-1) XNOR D(i);
           end loop;
-          q_m(8) = 0;
+          q_m(8) := '0';
         else 
           --XOR branch
-          q_m(0) = D(0);
+          q_m(0) := D(0);
           for i in 1 to 7 loop
-            q_m(i) = q_m(i-1) XOR D(i);
+            q_m(i) := q_m(i-1) XOR D(i);
           end loop;
-          q_m(8) = 1;
+          q_m(8) := '1';
         end if;
 
         if DE = '1' then
           N_1_q_m := getNbrOfOnes(q_m(7 downto 0));
           q_out(8) <= q_m(8);
 
-          if Cnt == 0 OR N_1_q_m = 4 then
+          if Cnt = 0 OR N_1_q_m = 4 then
             q_out(9) <= not(q_m(8));
             if q_m(8) = '1' then
               q_out(7 downto 0) <= q_m(7 downto 0);
@@ -82,12 +82,20 @@ begin
           else
             if (Cnt > 0 AND N_1_q_m > 4) OR (Cnt < 0 AND N_1_q_m < 4) then
               q_out(9) <= '1';
-              q_out(7 downto 0) = not(q_m(7 downto 0));
-              Cnt <= Cnt + q_m(8) + q_m(8) + (8-N_1_q_m) - N_1_q_m;
+              q_out(7 downto 0) <= not(q_m(7 downto 0));
+              if q_m(8) = '1' then
+                Cnt <= Cnt + 2 + (8-N_1_q_m) - N_1_q_m;
+              else
+                Cnt <= Cnt + (8-N_1_q_m) - N_1_q_m;
+              end if;
             else
               q_out(9) <= '0';
-              q_out(7 downto 0) = q_m(7 downto 0);
-              Cnt <= Cnt - not(q_m(8)) - not(q_m(8)) + N_1_q_m - (8-N_1_q_m);
+              q_out(7 downto 0) <= q_m(7 downto 0);
+              if q_m(8) = '0' then 
+                  Cnt <= Cnt - 2 + N_1_q_m - (8-N_1_q_m);
+              else
+                  Cnt <= Cnt + N_1_q_m - (8-N_1_q_m);
+              end if;
             end if;
           end if;
         else
@@ -99,10 +107,8 @@ begin
             when "11" => q_m := "1101010101";
             when others => q_m := "1101010101";
           end case;
-          q_m := "1101010100";
         end if;
       end if;
-      q_out <= q_m;
     end if;
   end process;
 
