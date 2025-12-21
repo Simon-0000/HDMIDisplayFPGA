cb10ef8c4b52cfaf103864cdb1c24193813f9116
SimonAsmar <simon.asmar@polymtl.ca>
2025-12-21 18:59:38 -0500

    fixed logic errors, missing library and missing assignments

--- TMDS_encoder/src/TMDS_encoder.vhd
+++ TMDS_encoder/src/TMDS_encoder.vhd
@@ -1,19 +1,11 @@
 library ieee; 
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
+
+--variable names and the state machine comes from the DVI specs, page 29: https://glenwing.github.io/docs/DVI-1.0.pdf
 entity TMDS_encoder is 
---  generic(
---    H_ACTIVE : positive := 640;
---    H_FRONT_PORCH : positive := 16;
---    H_SYNC : positive := 96;
---    H_BACK_PORCH : positive := 48;
-
---    V_ACTIVE : positive := 640;
---    V_FRONT_PORCH : positive := 16;
---    V_SYNC : positive := 96;
---    V_BACK_PORCH : positive := 48
---  );
-  port(--variable names and the state machine comes from the DVI specs, page 29: https://glenwing.github.io/docs/DVI-1.0.pdf
+  port(
     clk : in std_logic;
     rst : in std_logic;
     D : in std_logic_vector(7 downto 0);
@@ -38,7 +30,7 @@ architecture TMDS_encoder_arch of TMDS_encoder is
   end function;
 
 begin
-  process(clk, rst)
+  process(clk)
   variable q_m : std_logic_vector(9 downto 0);
   variable N_1_D : natural range 0 to 8 := 0;
   variable N_1_q_m : natural range 0 to 8 := 0;
@@ -67,6 +59,7 @@ begin
         end if;
 
         if DE = '1' then
+          --Drawing Period
           N_1_q_m := getNbrOfOnes(q_m(7 downto 0));
           q_out(8) <= q_m(8);
 
@@ -99,13 +92,14 @@ begin
             end if;
           end if;
         else
+          --Blanking period
           Cnt <= 0;
           case C1_C0 is 
-            when "00" => q_m := "0010101011";
-            when "01" => q_m := "1101010100";
-            when "10" => q_m := "0010101010";
-            when "11" => q_m := "1101010101";
-            when others => q_m := "1101010101";
+            when "00" => q_out <= "0010101011";
+            when "01" => q_out <= "1101010100";
+            when "10" => q_out <= "0010101010";
+            when "11" => q_out <= "1101010101";
+            when others => q_out <= "1101010101";
           end case;
         end if;
       end if;
