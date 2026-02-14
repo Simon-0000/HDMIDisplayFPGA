e6b0fb757db65a6cc062721f8218366f4e123f2b
SimonAsmar <simon.asmar@polymtl.ca>
2026-02-14 09:23:53 -0500

    Added lightweight rtos project to port existing code

---
+++ m3_project/src/library/middlewares/uart/gmd/retarget.c
@@ -0,0 +1,60 @@
+/*
+ ******************************************************************************************
+ * @file      retarget.c
+ * @author    GowinSemiconductor
+ * @device    Gowin_EMPU(GW1NS-4C)
+ * @brief     UART printf and scanf retarget function.
+ ******************************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gw1ns4c.h"
+#include <stdio.h>
+#include <sys/stat.h>
+
+__attribute__ ((used))  int _write (int fd, char *ptr, int len)
+{
+  size_t i;
+
+  for (i=0; i<len; i++)
+  {
+    UART_SendChar(UART0,ptr[i]); // call character output function
+  }
+
+  return len;
+}
+
+void _close(void)
+{
+
+}
+
+void _lseek(void)
+{
+
+}
+
+void _read(void)
+{
+
+}
+
+void _fstat(void)
+{
+
+}
+
+void _kill(void)
+{
+
+}
+
+void _isatty(void)
+{
+
+}
+
+void _getpid(void)
+{
+
+}
