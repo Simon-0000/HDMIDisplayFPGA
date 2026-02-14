bad071eb460181946b86438aab3b0e5d9b965bea
Simon Asmar <asmarsimon@hotmail.com>
2026-02-14 10:35:48 -0500

    Removed freertos project and converted to RTThread

--- m3_project/src/project/rtthread_nano/src/main.c
+++ m3_project/src/project/rtthread_nano/src/main.c
@@ -11,18 +11,66 @@
 #include <rtthread.h>
 #include <stdio.h>
 #include "gw1ns4c.h"
+#include "uart.h"
 
+#define CHAR_BUFFER_SIZE 16
+#define SET_COLOR_CMD "setColor"
+
+
+uint8_t currentColor[3] = {0};
+char colorBuffer[CHAR_BUFFER_SIZE] = {0};
+volatile uint8_t colorBufferIndex = 0;
+
+
+static void setColor(const char* rgb_hex_buffer);
+static int handle_uart_command(const char* buffer, uint8_t size);
+static void UART0_Handler(void);
 
 /* Definitions ---------------------------------------------------------------*/
 
 int main(void)
 {
+	SystemInit();      //Initializes system clock
+	uart_init(UART0,   //Initializes UART0
+	          9600,   //Baudrate
+	          1,       //Tx
+	          1,       //Rx
+	          0,       //Tx interrupt
+	          1,       //Rx interrupt
+	          0,       //Tx overflow interrupt
+	          0);      //Rx overflow interrupt
+//	help_print();
+
+
+    rt_kprintf("hello :)\n");
+
 	while(1)
 	{
-		GPIO_ResetBit(GPIO0, GPIO_Pin_0);	//LED1 on
-		rt_thread_mdelay(500);
-		
-		GPIO_SetBit(GPIO0, GPIO_Pin_0);		//LED1 off
-		rt_thread_mdelay(500);
+		rt_thread_mdelay(10000);
 	}
 }
+
+
+static void setColor(const char* rgb_hex_buffer)
+{
+	char *endptr;
+	long rgb_value = strtol(rgb_hex_buffer, &endptr, 16);
+
+	char hex_str[16];
+	rt_snprintf(hex_str, sizeof(hex_str), "0x%06X\r\n", (unsigned int)rgb_value);
+	rt_kprintf("\r\n setColor=");
+	rt_kprintf(hex_str);
+}
+
+void setColor_cmd(int argc, char **argv)
+{
+    if (argc < 2)
+        rt_kprintf("Usage: setColor <RRGGBB>\n");
+    else if (strlen(argv[1]) != 6)
+
+        rt_kprintf("Error: Provide 6 hex characters <RRGGBB>\n");
+    else
+    	setColor(argv[1]);
+}
+
+MSH_CMD_EXPORT_ALIAS(setColor_cmd, setColor, Set RGB color RRGGBB);
