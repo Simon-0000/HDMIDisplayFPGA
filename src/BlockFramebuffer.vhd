4753ca60b097b7ad6b80c49a42b1d6fa3a01c790
Simon Asmar <asmarsimon@hotmail.com>
2026-07-21 18:48:07 -0400

    Semi working blockframebuffer read and writes (offset and missing pixels left to fix)

--- src/BlockFramebuffer.vhd
+++ src/BlockFramebuffer.vhd
@@ -21,10 +21,9 @@ entity BlockFramebuffer is
     I_rd_data: in std_logic_vector(31 downto 0);
     I_init_calib: in std_logic;
 
-
     I_vin_clk: in std_logic;
     I_vin_wr_enable: in std_logic; 
-    I_vin_data: in std_logic_vector(31 downto 0); -- 2 rgb555 emcoded colors with the MSB of each telling us is we dont write to memoryTells us if we leave the memory untouched (skip to next position)
+    I_vin_data: in std_logic_vector(31 downto 0); 
 
     I_vout_clk: in std_logic;
     I_vout_vs_hs: in std_logic_vector(1 downto 0);
@@ -63,23 +62,6 @@ component block_framebuffer_FIFO
 	);
 end component;
 
-
-
---type state_type is (IDLE, SETUP_PIXEL, WRITE_PIXEL);
-
---constant BLOCK_COUNT_H: positive := H_ACTIVE/BLOCK_SIZE;
---constant BLOCK_COUNT_V: positive := V_ACTIVE/BLOCK_SIZE;
---signal block_index: natural range 0 to (BLOCK_COUNT_H * BLOCK_COUNT_V) - 1:= 0;
---signal pixel_pair_index: natural range 0 to ((BLOCK_SIZE * BLOCK_SIZE) / 2) - 1 := 0;
---signal pixel_pair_memory_index: natural := 0;
---signal state: state_type := IDLE;
---signal pixel_write_request: std_logic := '0';
-
---constant arbiter_cmd: std_logic := '1';
---signal arbiter_cmd_en: std_logic := '0';
---signal arbiter_addr: std_logic_vector(21 downto 0) := (others => '0');
---signal arbiter_wr_data: std_logic_vector(31 downto 0) := (others => '0');
---constant arbiter_data_mask: std_logic_vector(3 downto 0) := "0000";
 type mem_state_type is (RW_DECISION, READ_BURST, PRE_WRITE_BURST, WRITE_BURST);
 signal mem_state : mem_state_type := RW_DECISION;
 signal vin_fifo_RdEn: std_logic := '0';
@@ -103,7 +85,6 @@ signal mem_write_priority : unsigned(4 downto 0);
 signal mem_read_priority : unsigned(4 downto 0);
 signal rw_alternate : std_logic := '0';
 
--- vin and vout indexes
 constant NBR_OF_WORDS : positive := H_ACTIVE * V_ACTIVE / 2;
 constant WORDS_PER_BLOCK : positive := BLOCK_SIZE * BLOCK_SIZE;
 constant NBR_OF_BLOCKS : positive := NBR_OF_WORDS / WORDS_PER_BLOCK;
@@ -117,7 +98,6 @@ signal mem_reset_write_block_index : std_logic := '1';
 signal mem_write_block_y_pixel_index : unsigned(MEM_WRITE_BLOCK_PIXEL_INDEX_MSB downto 0) := (others => '0');
 signal mem_write_addr : unsigned(21 downto 0) := (others => '0');
 signal mem_burst_index : unsigned(MEM_BURST_INDEX_MSB downto 0) := (others => '0');
-signal mem_burst_is_active : std_logic := '0';
 
 begin
   vin_fifo: block_framebuffer_FIFO
@@ -154,6 +134,7 @@ begin
   O_vout_fifo_empty <= vout_fifo_Empty;
   
   mem_read_priority(0) <= not(rw_alternate);
+  mem_write_priority(0) <= rw_alternate;
   mem_write_priority(1) <= vin_fifo_Almost_Empty and not(vin_fifo_Empty);
   mem_read_priority(1) <= vout_fifo_Almost_Full and not(vout_fifo_Full);
   mem_write_priority(2) <= not(vin_fifo_Almost_Empty) and not(vin_fifo_Almost_Full);
@@ -162,8 +143,8 @@ begin
   mem_read_priority(3) <= vout_fifo_Almost_Empty;
   mem_write_priority(4) <= vin_fifo_Full;
   mem_read_priority(4) <= vout_fifo_Empty;
+  
   process(I_dma_clk)
-
   begin
     if rising_edge(I_dma_clk) then
       if I_rst = '1' then
@@ -187,18 +168,18 @@ begin
         if I_init_calib = '1' then
           if mem_state = RW_DECISION then 
             rw_alternate <= not rw_alternate;
-            if (mem_read_priority > mem_write_priority) and (vout_fifo_Almost_Full = '0') then -- Request read from memory
+            if (mem_read_priority > mem_write_priority) and (vout_fifo_Almost_Full = '0') then
               mem_state <= READ_BURST;
               O_cmd <= '0';
               O_cmd_en <= '1';
-              O_addr <= std_logic_vector(resize(mem_read_word_index, O_addr'length));
+              O_addr <= std_logic_vector(resize(mem_read_word_index * 4, O_addr'length));
               if mem_read_word_index >= NBR_OF_WORDS - MEM_BURST_NUM then
                 mem_read_word_index <= (others => '0');
               else
                 mem_read_word_index <= mem_read_word_index + MEM_BURST_NUM;
               end if;
 
-            elsif (mem_write_priority > mem_read_priority) and (vin_fifo_Almost_Empty = '0') then -- Write what is in vin fifo and request read for next iteration
+            elsif (mem_write_priority > mem_read_priority) and (vin_fifo_Almost_Empty = '0') then 
               if mem_reset_write_block_index = '1' then 
                 mem_reset_write_block_index <= '0';
                 mem_write_addr <= unsigned(vin_fifo_data_out(21 downto 0));
@@ -208,12 +189,13 @@ begin
                 mem_state <= PRE_WRITE_BURST;
               end if;
             end if;  
+            
           elsif mem_state = PRE_WRITE_BURST then
-            vin_fifo_RdEn <= '1';
+            vin_fifo_RdEn <= '0'; 
             mem_state <= WRITE_BURST;
 
           elsif mem_state = READ_BURST then
-            if I_rd_data_valid = '1' then -- Request write to vout fifo
+            if I_rd_data_valid = '1' then 
               vout_fifo_WrEn <= '1';
               vout_fifo_data_in <= I_rd_data;
               if mem_burst_index = MEM_BURST_NUM - 1 then
@@ -223,15 +205,17 @@ begin
                 mem_burst_index <= mem_burst_index + 1;
               end if;
             end if;
+            
           elsif mem_state = WRITE_BURST then
             if mem_burst_index = 0 then
               O_cmd <= '1';
               O_cmd_en <= '1';
-              O_addr <= std_logic_vector(mem_write_addr);
+              O_addr <= std_logic_vector(resize(mem_write_addr * 4, O_addr'length));
             end if;
             
             O_wr_data <= vin_fifo_data_out;
             O_data_mask <= (others => '0'); 
+            vin_fifo_RdEn <= '1'; 
             
             if mem_burst_index = MEM_BURST_NUM - 1 then
               mem_burst_index <= (others => '0');
@@ -245,7 +229,6 @@ begin
               end if;
             else
               mem_burst_index <= mem_burst_index + 1;
-              vin_fifo_RdEn <= '1';
             end if;
           end if;
         end if;
@@ -267,6 +250,7 @@ begin
   end process;
 
   vout_fifo_RdEn <= I_vout_de and vout_read_low_bits; 
+  
   process(I_vout_clk)
   begin
     if rising_edge(I_vout_clk) then
@@ -279,60 +263,5 @@ begin
       O_vout_de    <= I_vout_de;
     end if;
   end process;
--- process (apb_master_clk)
---  begin
---    if rising_edge(apb_master_clk) then
---      if apb_master_prst = '0' then
---        pixel_write_request <= '0';
---        arbiter_wr_data <= (others =>'0');
---        block_index <= 0;
---        pixel_pair_index <= 0;
---        pixel_pair_memory_index <= 0;
---      else
---        pixel_write_request <= '0';
---        if apb_master_pselX = '1' and apb_master_penable = '1' and apb_master_pwrite = '1' and state = IDLE then
---          if apb_master_paddr(7 downto 0) = APB_PIXEL_VALUE_ADDR then
---            pixel_pair_memory_index <= (block_index * ((BLOCK_SIZE * BLOCK_SIZE) / 2)) + pixel_pair_index;
---            pixel_pair_index <= pixel_pair_index + 1;
---            pixel_write_request <= '1';
---            arbiter_wr_data <= apb_master_pwdata;
---          elsif apb_master_paddr(7 downto 0) = APB_PIXEL_INDEX_ADDR then
---            block_index <= to_integer(unsigned(apb_master_pwdata));
---            pixel_pair_index <= 0;
---          end if;
---        end if;
---      end if;
---    end if;
---  end process;
 
-
---  process (apb_master_clk) -- State machine
---  begin
---    if rising_edge(apb_master_clk) then
---      if apb_master_prst = '0' then
---        state <= IDLE;
---      else
---        case state is
---          when IDLE =>
---            arbiter_cmd_en <= '0';
---            if pixel_write_request = '1' then
---              state <= SETUP_PIXEL;
---            end if;
---          when SETUP_PIXEL => 
---            state <= WRITE_PIXEL;
---            arbiter_addr <= std_logic_vector(to_unsigned(pixel_pair_memory_index, 22));
---          when WRITE_PIXEL =>
---            arbiter_cmd_en <= '1';
---            state <= IDLE;
---        end case;
---      end if;
---    end if;
---  end process;
---  apb_master_preadyX <= '1' when (state = IDLE) else '0';
-
---  hyperram_cmd       <= arbiter_cmd when (state /= IDLE) else framebuffer_cmd;
---  hyperram_cmd_en    <= arbiter_cmd_en when (state /= IDLE) else framebuffer_cmd_en;
---  hyperram_addr      <= arbiter_addr when (state /= IDLE) else framebuffer_addr;
---  hyperram_wr_data   <= arbiter_wr_data when (state /= IDLE) else framebuffer_wr_data;
---  hyperram_data_mask <= arbiter_data_mask when (state /= IDLE) else framebuffer_data_mask;
-end architecture structural; 
+end architecture structural;
